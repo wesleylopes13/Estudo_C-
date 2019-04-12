8bc86be0f1547f62e7e1dab2ced7bd86b95ae4e1
Wesley Lopes <wesleyalmeida1100@gmail.com>
2019-04-12 11:55:40 -0300

    Projeto iniciado - EstoqueTV

---
+++ EstoqueTV/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstoqueTV
+{
+    class Produto
+    {
+        public String Nome;
+        public double Valor;
+        public int Qtd;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Valor * Qtd;
+        }
+
+        public void AdicionarProduto()
+        {            
+            Console.Write("\n\nDigite a quantidade de produtos a serem inseridos: ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            this.Qtd += quantidade;
+        }
+
+        public void RemoverProduto()
+        {
+            int quantily = 0;
+
+            Console.Write("\n\nDigite a quantidade de produtos a serem removidos: ");
+            quantily = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            this.Qtd -= quantily;            
+        }
+
+        public void Exibir()
+        {
+
+            Console.Write("\nProduto: " + Nome +
+                "\nQuantidade: " + Qtd +
+                 "\nValor total em estoque: R$" + ValorTotalEmEstoque());
+        }
+    }
+}
---
+++ EstoqueTV/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstoqueTV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando Classe Produto
+
+            Produto prod;
+            prod = new Produto();
+
+            // Inserindo dados do produto
+
+            Console.Write("Digito o produto: ");
+            prod.Nome = Console.ReadLine();
+
+            Console.Write("\nDigite o preço do produto: ");
+            prod.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("\nDigite a quantidade de produtos no estoque: ");
+            prod.Qtd = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Exibindo os dados cadastrados
+            prod.Exibir();
+            
+            // Instanciando método para adicionar produto
+            prod.AdicionarProduto();
+            prod.Exibir();
+            
+            // Instaciando método para remover produto
+            prod.RemoverProduto();
+            prod.Exibir();
+                       
+            // Manter o console aberto
+            Console.ReadLine();
+        }
+    }
+}
